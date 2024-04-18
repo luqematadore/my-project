7da43e7b4d0fc08c97a7975729174bb5a0a0efac
luqematadore <164449687+luqematadore@users.noreply.github.com>
2024-04-18 14:43:06 +0800

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -10,3 +10,20 @@ BEGIN
     END IF;
 END; //
 DELIMITER ;
+
+/*trigger for return to all table parts*/
+
+DELIMITER $$
+
+CREATE TRIGGER insert_into_partitions
+AFTER INSERT ON wp_metforms
+FOR EACH ROW
+BEGIN
+    INSERT IGNORE INTO wp_ebss_part_1 (column1, column2) VALUES (NEW.column1, NEW.column2);
+    INSERT IGNORE INTO wp_ebss_part_2_1 (column1, column2) VALUES (NEW.column1, NEW.column2);
+    INSERT IGNORE INTO wp_ebss_part_2_1 (column1, column2) VALUES (NEW.column1, NEW.column2);
+    INSERT IGNORE INTO wp_ebss_part_3 (column1, column2) VALUES (NEW.column1, NEW.column2);
+    INSERT IGNORE INTO wp_ebss_part_4 (column1, column2) VALUES (NEW.column1, NEW.column2);
+END$$
+
+DELIMITER ;
