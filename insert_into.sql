cd45a383e59c53bf75a989b78124e5abad15a15a
luqematadore <164449687+luqematadore@users.noreply.github.com>
2024-04-23 01:03:56 +0800

    Update insert_into.sql

--- insert_into.sql
+++ insert_into.sql
@@ -1,3 +1,5 @@
+/* PART 1*/
+
 INSERT INTO wp_ebss_part_1 (user_id, email_address, full_name, first_name, last_name, staff_ID, clientName, company_email, job_title, gender_type, work_location, department, work_section, year_service)
 SELECT 
    m.user_id,
@@ -16,3 +18,32 @@ SELECT
    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:12:"year_service";s:12:"', -1), '";}', 1), '"', -1) AS year_service
 FROM wp_metforms m
 WHERE m.user_id = 9402;
+
+/* PART 2_1 */
+INSERT INTO wp_ebss_part_2 (user_id, laptop, standardPC, external, dockingNE, dockingE, standardMW, standardMNW, ErgomouseNW, ErgomouseW, shortkey, longkey, riserA, riserNA, BagA, BagB, BagC, mousepadA, 
+   mousepadB, wristpadA, wristpadB)
+SELECT 
+    m.user_id,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:6:"laptop";s:', -1), '";s:10:"standardPC"', 1), '"', -1) AS laptop,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:10:"standardPC";s:', -1), '";s:8:"external"', 1), '"', -1) AS standardPC,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:8:"external";s:', -1), '";s:9:"dockingNE"', 1), '"', -1) AS external,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:9:"dockingNE";s:', -1), '";s:8:"dockingE"', 1), '"', -1) AS dockingNE,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:8:"dockingE";s:', -1), '";s:10:"standardMW"', 1), '"', -1) AS dockingE,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:10:"standardMW";s:', -1), '";s:11:"standardMNW"', 1), '"', -1) AS standardMW,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:11:"standardMNW";s:', -1), '";s:11:"ErgomouseNW"', 1), '"', -1) AS standardMNW,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:11:"ErgomouseNW";s:', -1), '";s:10:"ErgomouseW"', 1), '"', -1) AS ErgomouseNW,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:10:"ErgomouseW";s:', -1), '";s:8:"shortkey"', 1), '"', -1) AS ErgomouseW,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:8:"shortkey";s:', -1), '";s:7:"longkey"', 1), '"', -1) AS shortkey,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:7:"longkey";s:', -1), '";s:6:"riserA"', 1), '"', -1) AS longkey,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:6:"riserA";s:', -1), '";s:7:"riserNA"', 1), '"', -1) AS riserA,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:7:"riserNA";s:', -1), '";s:4:"BagA"', 1), '"', -1) AS riserNA,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:4:"BagA";s:', -1), '";s:4:"BagB"', 1), '"', -1) AS BagA,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:4:"BagB";s:', -1), '";s:4:"BagC"', 1), '"', -1) AS BagB,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:4:"BagC";s:', -1), '";s:9:"mousepadA"', 1), '"', -1) AS BagC,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:9:"mousepadA";s:', -1), '";s:9:"mousepadB"', 1), '"', -1) AS mousepadA,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:9:"mousepadB";s:', -1), '";s:9:"wristpadA"', 1), '"', -1) AS mousepadB,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:9:"wristpadA";s:', -1), '";s:9:"wristpadB"', 1), '"', -1) AS wristpadA,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(m.meta_value, 's:9:"wristpadB";s:', -1), '";}', 1), '"', -1) AS wristpadB
+FROM wp_metforms m
+WHERE m.user_id = 9398;
+
